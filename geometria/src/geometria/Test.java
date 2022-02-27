9c7e111cb76178cc88cd3c505de75acb9717faa4
sergio <castillovalerosergio@gmail.com>
2022-02-27 18:32:18 +0100

    Uff ahora si

---
+++ geometria/src/geometria/Test.java
@@ -0,0 +1,17 @@
+package geometria;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		double radio = -3;
+		double PI = 3.1416;
+		System.out.println( radio * radio * PI);
+		System.out.println(radio * 2 * PI);
+		
+		fail("No implementado aun");
+	}
+
+}
