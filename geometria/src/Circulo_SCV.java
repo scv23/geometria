1a31607303fddc752ad94468af6cd21a041d092e
sergio <castillovalerosergio@gmail.com>
2022-02-27 17:51:37 +0100

    versión refactorizada

--- geometria/src/Circulo_SCV.java
+++ geometria/src/Circulo_SCV.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_SCV extends FiguraGeometrica_SCV {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_SCV(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_SCV.java
+++ geometria/src/FiguraGeometrica_SCV.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_SCV {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_SCV(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
